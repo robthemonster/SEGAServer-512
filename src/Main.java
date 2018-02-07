8110f3cd7868f878e648771f3f46cc6577cda3c0
robthemonster <robthemonsterlevin@gmail.com>
2018-02-07 03:13:38 -0500

    Server responds to client.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,14 +7,25 @@ public class Main {
         try {
             ServerSocket serverSocket = new ServerSocket(6969);
             InputStreamReader inputStreamReader;
+            OutputStreamWriter outputStreamWriter;
             BufferedReader bufferedInputStream;
+            PrintWriter printWriter;
             Socket socket;
             while (true){
                 socket = serverSocket.accept();
                 inputStreamReader = new InputStreamReader(socket.getInputStream());
                 bufferedInputStream = new BufferedReader(inputStreamReader);
                 System.out.println(bufferedInputStream.readLine());
+                System.out.println("Remote Address :" + socket.getRemoteSocketAddress());
+                Socket client = new Socket(socket.getInetAddress(), 6969);
+                outputStreamWriter = new OutputStreamWriter(client.getOutputStream());
+                printWriter = new PrintWriter(outputStreamWriter);
+                printWriter.print("Message received by server.");
+                printWriter.flush();
+                printWriter.close();
                 bufferedInputStream.close();
+                socket.close();
+                client.close();
             }
         } catch (IOException e){
             e.printStackTrace();
