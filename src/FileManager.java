165381ec78e1a6eb2816c6b874fe5dfb4b92158a
robthemonster <robthemonsterlevin@gmail.com>
2018-03-25 22:29:24 -0400

    Added FTP server. deleted some old runnables. cleaned up Main

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,8 +1,6 @@
 import SEGAMessages.*;
-import org.apache.commons.io.FileUtils;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class FileManager {
@@ -35,55 +33,6 @@ public class FileManager {
             }
         }
         response.setFiles(result);
-        return response;
-    }
-
-    public static GetFileFromGroupResponse getFileFromGroup(GetFileFromGroupRequest request) {
-        GetFileFromGroupResponse response = new GetFileFromGroupResponse();
-        if (!DatabaseManager.userIsInGroup(request.getUsername(), request.getGroupname())) {
-            response.setErrorMessage("User is not in that group.");
-            return response;
-        }
-        File file = new File(groupSubfolder + File.separator + request.getGroupname() + File.separator + request.getFilename());
-        if (file.exists()) {
-            try {
-                response.setFile(FileUtils.readFileToByteArray(file));
-            } catch (IOException e) {
-                e.printStackTrace();
-                Logger.debug(e.getMessage());
-                response.setErrorMessage("Error writing file to response");
-            }
-        } else {
-            response.setErrorMessage("File does not exist");
-        }
-        return response;
-    }
-
-    public static UploadFileToGroupResponse uploadFileToGroup(UploadFileToGroupRequest request) {
-        UploadFileToGroupResponse response = new UploadFileToGroupResponse();
-        if (!DatabaseManager.userIsInGroup(request.getUsername(), request.getGroupname())) {
-            response.setErrorMessage("User is not in that group.");
-            return response;
-        }
-        try {
-            File file = new File(groupSubfolder + File.separator + request.getGroupname() + File.separator + request.getFilename());
-            if (!file.getParentFile().exists()) {
-                if (!file.getParentFile().mkdirs()) {
-                    Logger.debug("WHAT HAPPENED");
-                }
-            }
-            if (file.createNewFile()) {
-                FileUtils.writeByteArrayToFile(file, request.getFile());
-                response.setSucceeded(true);
-            } else {
-                response.setErrorMessage("Error creating file");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            Logger.debug(e.getMessage());
-            response.setErrorMessage("Error uploading file.");
-            response.setSucceeded(false);
-        }
         return response;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,95 @@
 import SEGAMessages.*;
+import org.apache.ftpserver.ConnectionConfigFactory;
+import org.apache.ftpserver.DataConnectionConfigurationFactory;
+import org.apache.ftpserver.FtpServer;
+import org.apache.ftpserver.FtpServerFactory;
+import org.apache.ftpserver.ftplet.FtpException;
+import org.apache.ftpserver.listener.Listener;
+import org.apache.ftpserver.listener.ListenerFactory;
+import org.apache.ftpserver.usermanager.impl.BaseUser;
+import org.apache.ftpserver.usermanager.impl.ConcurrentLoginPermission;
+import org.apache.ftpserver.usermanager.impl.WritePermission;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("SEGA SERVER HAS BEGUN");
+
         try {
+            Thread ftpServer = getFtpServerThread();
+            Thread messageHandler = getMessageHandlerThread();
             Logger.startLogger(System.currentTimeMillis());
             Logger.debug("Logger started");
-            ServerSocket serverSocket = new ServerSocket(6969);
-            Socket socket;
-            while (true) {
-                socket = serverSocket.accept();
-                handleMessage(socket);
-                socket.close();
-            }
-        } catch (IOException e) {
+            messageHandler.setPriority(Thread.MAX_PRIORITY);
+            ftpServer.setPriority(Thread.NORM_PRIORITY);
+            ftpServer.start();
+            messageHandler.start();
+
+        } catch (IOException | FtpException e) {
             e.printStackTrace();
             Logger.debug(e.getMessage());
 
         }
+    }
+
+    private static Thread getFtpServerThread() throws FtpException {
+        FtpServerFactory serverFactory = new FtpServerFactory();
+
+        ConnectionConfigFactory connectionConfigFactory = new ConnectionConfigFactory();
+        connectionConfigFactory.setAnonymousLoginEnabled(true);
+        connectionConfigFactory.setMaxThreads(10);
+        DataConnectionConfigurationFactory dataConnectionConfigurationFactory = new DataConnectionConfigurationFactory();
+        dataConnectionConfigurationFactory.setPassivePorts("6921-6931");
+        ListenerFactory listenerFactory = new ListenerFactory();
+        listenerFactory.setPort(6921);
+        listenerFactory.setDataConnectionConfiguration(dataConnectionConfigurationFactory.createDataConnectionConfiguration());
+        BaseUser anon = new BaseUser();
+        anon.setName("anon");
+        anon.setHomeDirectory(System.getProperty("user.dir"));
+        anon.setAuthorities(Arrays.asList(new WritePermission(), new ConcurrentLoginPermission(10, 1)));
+        TreeMap<String, Listener> listenerTreeMap = new TreeMap<>();
+        listenerTreeMap.put("default", listenerFactory.createListener());
+        serverFactory.setConnectionConfig(connectionConfigFactory.createConnectionConfig());
+        serverFactory.setListeners(listenerTreeMap);
+        serverFactory.getUserManager().save(anon);
+        return new Thread(() -> {
+            try {
+                FtpServer server = serverFactory.createServer();
+                server.start();
+                while (!server.isStopped()) {
+                    if (server.isSuspended()) {
+                        System.out.println("ftp suspended");
+                    }
+                }
+
+            } catch (FtpException e) {
+                e.printStackTrace();
+                Logger.debug(e.getMessage());
+            }
+        });
+    }
+
+    private static Thread getMessageHandlerThread() throws IOException {
+        final ServerSocket serverSocket = new ServerSocket(6969);
+        return new Thread(() -> {
+            try {
+                Socket socket;
+                while (true) {
+                    socket = serverSocket.accept();
+                    handleMessage(socket);
+                    socket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Logger.debug(e.getMessage());
+            }
+        });
     }
 
     private static void handleMessage(Socket socket) {
@@ -64,14 +131,8 @@ public class Main {
         if (request instanceof AddUserToGroupRequest) {
             return new AddUserToGroupRunnable((AddUserToGroupRequest) request);
         }
-        if (request instanceof GetFileFromGroupRequest) {
-            return new GetFileFromGroupRunnable((GetFileFromGroupRequest) request);
-        }
         if (request instanceof GetFilesForGroupRequest) {
             return new GetFilesForGroupRunnable((GetFilesForGroupRequest) request);
-        }
-        if (request instanceof UploadFileToGroupRequest) {
-            return new UploadFileToGroupRunnable((UploadFileToGroupRequest) request);
         }
         if (request instanceof DeleteFileFromGroupRequest) {
             return new DeleteFileFromGroupRunnable((DeleteFileFromGroupRequest) request);
